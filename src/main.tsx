c67bc23417018fb497cd34ff8907bf58d8b812b5
Simon <twop.sk@gmail.com>
2019-12-05 00:18:11 -0800

    test for gh actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import { render, h } from "preact";
 /** @jsx h */
 
-render(<div>Hello world</div>, document.getElementById("root")!);
+render(<div>Hello world, test for gh actions</div>, document.getElementById("root")!);
