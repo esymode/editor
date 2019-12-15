e3e6f4808fdf27e13e040eeaa760543c32611534
Sean Stephens <seanastephens@email.arizona.edu>
2019-12-14 22:33:29 -0700

    Add preview component

---
+++ src/preview.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { useRef, useEffect } from "react";
+
+const putSourceIntoIframe = async function(
+  iframe: HTMLIFrameElement,
+  source: string
+) {
+  iframe.src = "";
+
+  iframe.onload = async () => {
+    const iframeDoc = iframe.contentDocument!;
+
+    const srcScript = (src: string): HTMLScriptElement => {
+      const script = iframeDoc.createElement("script");
+      script.innerHTML = src;
+      return script;
+    };
+
+    iframeDoc.body.appendChild(
+      (() => {
+        const div = iframeDoc.createElement("div");
+        div.id = "root";
+        return div;
+      })()
+    );
+    iframeDoc.head.appendChild(srcScript(source));
+  };
+};
+
+export const Preview: React.FC<{ source: string }> = ({ source }) => {
+  const outputRef = useRef<HTMLIFrameElement | null>(null);
+
+  useEffect(() => {
+    if (outputRef.current) {
+      putSourceIntoIframe(outputRef.current, source);
+    }
+  }, [source]);
+
+  return (
+    <iframe
+      ref={outputRef}
+      id="output"
+      style={{ width: "100%", height: "350px", border: "1px solid grey" }}
+    ></iframe>
+  );
+};
