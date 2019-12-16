578e54ce9e6145fd274816861a48725337c58c69
Simon Korzunov <twop.sk@twopmini.local>
2019-12-16 00:34:20 -0800

    WIP data modeling for side panel and main app

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import { IDE } from "./Editor";
 
 // doPackageResolution({ react: "^16.9.0", "react-dom": "^16.9.0" }).then(res => {
 //   console.log(res);
+
+//   //   const project = {
+//   //       file:'index.js',
+//   //       content: `
+//   //       import {foo} from './foo.js';
+//   //       console.log(foo);`
+//   //   }
 // });
 
 console.log({ IDE });
