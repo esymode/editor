fb8f91916e509424f4ff1b0594b7aa49bc137f2e
Sean Stephens <seanastephens@email.arizona.edu>
2020-01-20 23:53:49 -0700

    Delete old comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 import { App } from "./App";
-// import { doPackageResolution } from "./packaging/doResolution";
-
-// doPackageResolution({ react: "^16.9.0", "react-dom": "^16.9.0" }).then(res => {
-//   console.log(res);
-
-//   //   const project = {
-//   //       file:'index.js',
-//   //       content: `
-//   //       import {foo} from './foo.js';
-//   //       console.log(foo);`
-//   //   }
-// });
 
 ReactDOM.render(<App />, document.getElementById("root")!);
