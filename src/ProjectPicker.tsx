bbfe1fe9724de40c906324cd59945a94280e8a1b
Simon Korzunov <twop.sk@twopmini.local>
2020-02-01 21:57:41 -0800

    first working elm architecture impl

--- src/ProjectPicker.tsx
+++ src/ProjectPicker.tsx
@@ -4,67 +4,82 @@ import { css } from "emotion";
 
 import { ProjectDesc } from "shared/client_server_api";
 import { Union, of } from "ts-union";
-import { Thunk, useThunk } from "./shared/useThunk";
-import { useReducer, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { Horizontal } from "./styles";
 import { APIClient, ApiContext } from "./ApiContext";
 import { Link } from "./NavigationPrimitives";
 import { projectEditingRoute } from "./routes";
 import { toPersistantForm, createModelWithIndexTS } from "./projectModel";
+import {
+  declareEffects,
+  effect,
+  UpdateFunction,
+  cmd,
+  AnyCommand,
+  EffectHandler
+} from "./shared/stateMachine";
+import { useMachine } from "./shared/useMachine";
 
 const State = Union({
   Loading: of(null),
   Loaded: of<ProjectDesc[]>()
 });
+const { Loaded, Loading } = State;
 
 type State = typeof State.T;
 
 const Ev = Union({
   LoadProjects: of<ProjectDesc[]>(),
   AddProject: of<ProjectDesc>(),
-  DeleteAll: of(null)
+  DeleteAll: of(null),
+  RequestDeleteAll: of(null),
+  RequestNewProject: of<string>()
 });
 
 type Ev = typeof Ev.T;
 
-const update = (prev: State, ev: Ev): State =>
-  State.match(prev, {
-    Loading: () => Ev.if.LoadProjects(ev, projects => State.Loaded(projects)),
-    Loaded: projects =>
-      Ev.match(ev, {
-        LoadProjects: () => undefined,
-        AddProject: proj => State.Loaded(projects.concat(proj)),
-        DeleteAll: () => State.Loaded([])
-      })
-  }) ?? prev;
+const Effects = declareEffects({
+  fetchProjects: effect<() => ProjectDesc[]>(),
+  createProject: effect<(name: string) => ProjectDesc>(),
+  deleteAllProjects: effect<() => boolean>()
+});
 
-const createProject = (
-  client: APIClient,
-  name: string
-): Thunk<State, Ev> => async send =>
-  send(
-    Ev.AddProject(
-      await client.createProject(toPersistantForm(createModelWithIndexTS(name)))
-    )
-  );
-
-const deleteAllProjects = (
-  client: APIClient
-): Thunk<State, Ev> => async send => {
-  await client.clearProjects();
-  send(Ev.DeleteAll);
+const effectHandlers: EffectHandler<typeof Effects, APIClient> = {
+  fetchProjects: (_, client) => client.listProjects(),
+  createProject: (name, client) =>
+    client.createProject(toPersistantForm(createModelWithIndexTS(name))),
+  deleteAllProjects: (_, client) => client.clearProjects()
 };
 
-const fetchProjects = (client: APIClient): Thunk<State, Ev> => async send =>
-  send(Ev.LoadProjects(await client.listProjects()));
+const update: UpdateFunction<State, Ev, typeof Effects> = (prev, ev) =>
+  State.match(prev, {
+    Loading: () => [
+      Ev.if.LoadProjects(ev, projects => Loaded(projects)) ?? prev
+    ],
+
+    Loaded: projects =>
+      Ev.match<[State, AnyCommand<typeof Effects, Ev>?]>(ev, {
+        LoadProjects: () => [prev],
+        AddProject: proj => [Loaded(projects.concat(proj))],
+        DeleteAll: () => [Loaded([])],
+        RequestNewProject: name => [
+          prev,
+          cmd(Effects.createProject(name), Ev.AddProject)
+        ],
+        RequestDeleteAll: () => [
+          prev,
+          cmd(Effects.deleteAllProjects(), () => Ev.DeleteAll)
+        ]
+      })
+  });
 
 export const ProjectPicker: React.FC = () => {
-  const apiClient = useContext(ApiContext);
-  const [state, send] = useThunk(useReducer(update, State.Loading));
-
-  useEffect(() => {
-    send(fetchProjects(apiClient));
-  }, []);
+  const [state, send] = useMachine(
+    update,
+    () => [Loading, cmd(Effects.fetchProjects(), Ev.LoadProjects)],
+    effectHandlers,
+    useContext(ApiContext)
+  );
 
   // console.log("###", state);
 
@@ -78,13 +93,13 @@ export const ProjectPicker: React.FC = () => {
               onClick={() => {
                 const projectName = prompt("Project name?");
                 if (projectName) {
-                  send(createProject(apiClient, projectName));
+                  send(Ev.RequestNewProject(projectName));
                 }
               }}
             >
               Add project
             </button>
-            <button onClick={() => send(deleteAllProjects(apiClient))}>
+            <button onClick={() => send(Ev.RequestDeleteAll)}>
               Delete All
             </button>
             <ul>
